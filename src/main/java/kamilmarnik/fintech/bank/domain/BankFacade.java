4819cbe573fd17f9d2f3c72c880bf0aec9ca976a
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-05-10 15:11:20 +0200

    1

--- src/main/java/kamilmarnik/fintech/bank/domain/BankFacade.java
+++ src/main/java/kamilmarnik/fintech/bank/domain/BankFacade.java
@@ -18,31 +18,31 @@ import java.util.stream.Collectors;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public final class BankFacade {
 
-  BankRepository bankRepository;
+    BankRepository bankRepository;
 
-  public AccountDto createAccount(UUID accountId) {
-    bankRepository.findById(accountId)
-        .ifPresent(account -> { throw new InvalidAccountCreation(); });
-    final Account account = Account.create(accountId);
-    return bankRepository.save(account)
-        .dto();
-  }
+    public AccountDto createAccount(UUID accountId) {
+        bankRepository.findById(accountId)
+                .ifPresent(account -> {
+                    throw new InvalidAccountCreation();
+                });
+        final Account account = Account.create(accountId);
+        return bankRepository.save(account)
+                .dto();
+    }
 
-  public AccountDto withdraw(Withdrawal withdrawal) {
-    final Account account = getAccount(withdrawal.accountId());
-    bankRepository.save(account.withdraw(withdrawal.value()));
-    return account.dto();
-  }
+    public AccountDto withdraw(Withdrawal withdrawal) {
+        final Account account = getAccount(withdrawal.accountId());
+        return bankRepository.save(account.withdraw(withdrawal.value())).dto();
+    }
 
-  public AccountDto deposit(Deposit deposit) {
-    final Account account = getAccount(deposit.accountId());
-    bankRepository.save(account.deposit(deposit.value()));
-    return account.dto();
-  }
+    public AccountDto deposit(Deposit deposit) {
+        final Account account = getAccount(deposit.accountId());
+        return bankRepository.save(account.deposit(deposit.value())).dto();
+    }
 
-  private Account getAccount(UUID accountId) {
-    return bankRepository.findById(accountId)
-        .orElseThrow(() -> new AccountNotFound(accountId));
-  }
+    private Account getAccount(UUID accountId) {
+        return bankRepository.findById(accountId)
+                .orElseThrow(() -> new AccountNotFound(accountId));
+    }
 
 }
